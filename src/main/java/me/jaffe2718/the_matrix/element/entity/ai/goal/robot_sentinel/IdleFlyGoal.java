70833c48d636f7b5cd53f53538f9ed249c42a83f
Jaffe2718 <qqyttwqeei@163.com>
2023-12-07 17:40:48 +0800

    adjust Robot Sentinels' IdleFlyGoal.java

--- src/main/java/me/jaffe2718/the_matrix/element/entity/ai/goal/robot_sentinel/IdleFlyGoal.java
+++ src/main/java/me/jaffe2718/the_matrix/element/entity/ai/goal/robot_sentinel/IdleFlyGoal.java
@@ -2,8 +2,12 @@ package me.jaffe2718.the_matrix.element.entity.ai.goal.robot_sentinel;
 
 import me.jaffe2718.the_matrix.element.entity.mob.RobotSentinelEntity;
 import me.jaffe2718.the_matrix.unit.MathUnit;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+
+import java.util.List;
 
 public class IdleFlyGoal extends FlyMovementGoal {
 
@@ -33,6 +37,14 @@ public class IdleFlyGoal extends FlyMovementGoal {
     public void tick() {
         if (this.mob.getRandom().nextInt(100) == 0 || this.mob.isOnGround() || this.mob.isTouchingWater()) {
             this.adjustDirection();
+        } else {
+            if (this.isGetStuck()) {
+                if (this.mob.getRandom().nextBoolean()) {
+                    this.adjustDirection();
+                } else {
+                    this.invertDirection();
+                }
+            }
         }
         Vec3d velocity = this.calculateVelocity();
         this.mob.setVelocity(velocity);
@@ -42,7 +54,7 @@ public class IdleFlyGoal extends FlyMovementGoal {
     private void adjustDirection() {
         this.circlingCenter = this.mob.getBlockPos().add(
                 this.mob.getRandom().nextBetween(-25, 25),
-                this.mob.getRandom().nextBetween(-5, 5),
+                this.mob.getRandom().nextBetween(-10, 10),
                 this.mob.getRandom().nextBetween(-25, 25)
         );
         this.rotationVec = new Vec3d(
@@ -51,6 +63,12 @@ public class IdleFlyGoal extends FlyMovementGoal {
                 this.mob.getRandom().nextBetween(-5, 5)
         ).normalize().multiply(this.mob.getRandom().nextBetween(25, 40));
         // System.out.println("circlingCenter: " + this.circlingCenter + ", rotationVec: " + this.rotationVec);
+    }
+
+    private void invertDirection() {
+        this.rotationVec = this.rotationVec.multiply(-1);
+        BlockPos delta = this.mob.getBlockPos().subtract(this.circlingCenter);
+        this.circlingCenter = this.circlingCenter.subtract(delta.multiply(2));
     }
 
     /**
@@ -73,5 +91,29 @@ public class IdleFlyGoal extends FlyMovementGoal {
         } else {
             return relativeVec.normalize().multiply(1.2D);
         }
+    }
+
+    private boolean isGetStuck() {
+        Vec3d viewVec = this.mob.getRotationVector();
+        BlockPos frontPos = this.mob.getBlockPos().add((int) Math.round(viewVec.x), (int) Math.round(viewVec.y), (int) Math.round(viewVec.z));
+        List<BlockPos> checkPosList = List.of(
+                frontPos,
+                frontPos.up(),
+                frontPos.down(),
+                frontPos.north(),
+                frontPos.south(),
+                frontPos.east(),
+                frontPos.west()
+        );
+        boolean isStuck = false;
+        for (BlockPos checkPos : checkPosList) {
+            Block checkBlock = this.mob.getWorld().getBlockState(checkPos).getBlock();
+            if (checkBlock != Blocks.AIR && checkBlock != Blocks.CAVE_AIR &&
+                    checkBlock != Blocks.WATER && checkBlock != Blocks.WATER_CAULDRON) {
+                isStuck = true;
+                break;
+            }
+        }
+        return isStuck;
     }
 }
